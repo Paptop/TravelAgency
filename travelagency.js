adcc0c6bb6ef84ba7b89fe7b29246adecf5d8db6
Paptop <iljajurchenko@gmail.com>
2020-11-07 18:30:29 +0200

    Web: Handlebars and Express init

---
+++ travelagency.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express(); 
+var handlebars = require('express-handlebars').create({defaultlayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000); 
+
+app.get('/', function(req,res){
+    res.render('home');
+});
+
+app.get('/about', function(req,res){
+    res.render('about');
+});
+
+app.use(function(req, res){
+    res.status('404');
+    res.render('404');
+});
+
+app.use(function(req,res){
+    res.status('500');
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express running on http://localhost:' + app.get('port') + ': press Ctrl+C to quit');
+});
