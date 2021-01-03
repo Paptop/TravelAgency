3f2a023bf734598ba11cac0ca903abe521592a7d
Paptop <iljajurchenko@gmail.com>
2021-01-03 15:43:52 +0200

    Added populate script

---
+++ populate.js
@@ -0,0 +1,69 @@
+var model = require('./model')
+
+model.db_insert(
+    0,
+    "French Vacation",
+    "France, Paris",
+    "Entertainment",
+    10,
+    "SightSeeing"
+)
+
+model.db_insert(
+    0,
+    "Charming Haarlem",
+    "Amsterdam",
+    "Education",
+    30,
+    "SightSeeing"
+)
+
+model.db_insert(
+    1,
+    "Royal Sea",
+    "The Bahamas",
+    "Vacation",
+    300,
+    "Shore"
+)
+
+model.db_insert(
+    2,
+    "Caribbean Pirates",
+    "West Indies",
+    "Adventure",
+    200,
+    "Shore"
+)
+
+model.db_insert(
+    2,
+    "Caribbean Pirates",
+    "West Indies",
+    "Adventure",
+    200,
+    "Shore"
+)
+
+model.db_insert_prices(
+    "400$",
+    "300$",
+    "M223-02-001",
+    1
+)
+
+model.db_insert_prices(
+    "1000$",
+    "850$",
+    "W223-58-010",
+    2
+)
+
+model.db_insert_prices(
+    "700$",
+    "600$",
+    "C604-25-035",
+    2
+)
+
+process.exit(0)
